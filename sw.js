95ef4b1f42aee59bd4364b75afe67d5e9c58bb23
GIBB1994 <mitchellgibbons1@gmail.com>
2026-01-10 16:32:26 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,8 +2,10 @@ const CACHE_NAME = "simplespend-v0.7";
 const FILES = [
   "./",
   "./index.html",
+  "./css/styles.css",
+  "./js/app.js",
   "./manifest.json",
   "./assets/icon-192.png",
-  "./assets/icon-512.png"
+  "./assets/icon-512.png",
+  "./VERSION.txt"
 ];
-
