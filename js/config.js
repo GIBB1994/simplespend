4b167bdb2299fec341202b919a9c9f92be3421ca
GIBB1994 <mitchellgibbons1@gmail.com>
2026-01-11 01:20:47 -0600

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// js/config.js  (DO NOT COMMIT THIS FILE)
+window.SS_CONFIG = {
+  SUPABASE_URL: "https://kjxojovzpdofdcyyosqn.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqeG9qb3Z6cGRvZmRjeXlvc3FuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgxMDQwOTQsImV4cCI6MjA4MzY4MDA5NH0.HqHQ6JsH1FEDyO8xnUOI_CaW8skQkiG5dk3ShcMuxb4"
+};
