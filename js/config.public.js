07ae95734a8ee19114be51b18cdd7af5ad95b0f3
GIBB1994 <mitchellgibbons1@gmail.com>
2026-01-11 01:59:35 -0600

    Create config.public.js

---
+++ js/config.public.js
@@ -0,0 +1,7 @@
+// js/config.public.js (COMMITTED)
+window.SS_CONFIG = {
+  SUPABASE_URL: "https://oplfdgnhncdpqqxlovdt.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wbGZkZ25obmNkcHFxeGxvdmR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgxMDg3MzIsImV4cCI6MjA4MzY4NDczMn0.DbXygFGfQgGG2P1vAC95mxz_hdFwlZnjDO8LPwdrmQQ"
+};
+
+
