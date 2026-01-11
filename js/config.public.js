d93afbd32a47cba7b0e467d6c5b138d00921c662
GIBB1994 <mitchellgibbons1@gmail.com>
2026-01-11 03:25:08 -0600

    Temp Change to DEV

--- js/config.public.js
+++ js/config.public.js
@@ -1,7 +1,11 @@
 // js/config.public.js (COMMITTED)
 window.SS_CONFIG = {
-  SUPABASE_URL: "https://oplfdgnhncdpqqxlovdt.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wbGZkZ25obmNkcHFxeGxvdmR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgxMDg3MzIsImV4cCI6MjA4MzY4NDczMn0.DbXygFGfQgGG2P1vAC95mxz_hdFwlZnjDO8LPwdrmQQ"
+  // PROD
+  // SUPABASE_URL: "https://oplfdgnhncdpqqxlovdt.supabase.co",
+  // SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wbGZkZ25obmNkcHFxeGxvdmR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgxMDg3MzIsImV4cCI6MjA4MzY4NDczMn0.DbXygFGfQgGG2P1vAC95mxz_hdFwlZnjDO8LPwdrmQQ"
+  // DEV
+  SUPABASE_URL: "https://kjxojovzpdofdcyyosqn.supabase.co",
+  UPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqeG9qb3Z6cGRvZmRjeXlvc3FuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgxMDQwOTQsImV4cCI6MjA4MzY4MDA5NH0.HqHQ6JsH1FEDyO8xnUOI_CaW8skQkiG5dk3ShcMuxb4"
 };
 
 
